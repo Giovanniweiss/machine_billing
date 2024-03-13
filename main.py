3af6ae9481a2cb8e4c8891171536f5c6ce24ba50
Giovanni <Projeto6@EQUIMATEC.local>
2024-03-13 17:00:00 -0300

    Fixed function for non internal welds.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os, datetime, shutil
 if __name__ == "__main__":
     # Obter informações do usuário.
     lista, destino, acervo, pastas_de_solda, solda_usinados_interna = tg.abrir_GUI()
+    nome_planilha_csv = str(os.path.splitext(os.path.basename(lista))[0])
     if lista == "" or destino == "" or acervo == "":
         print("Input inválido.")
         exit()
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     # Criar pastas conforme necessário.
     current_time = datetime.datetime.now()
     current_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
-    folder_name = current_time + "_arquivos_orcamento"
+    folder_name = current_time + "_" + nome_planilha_csv
     destino = os.path.join(destino, folder_name)
     if not os.path.exists(destino):
         os.makedirs(destino)
@@ -26,6 +27,9 @@ if __name__ == "__main__":
     if data_list is not None:
         for i in range(min(5, len(data_list))):
             print(data_list[i])
+    else:
+        print("Falha de leitura do arquivo csv.")
+        exit()
     
     # Processamento 
     lista_billing, lista_avulsos = tb.billing_folders_and_list(data_list)
@@ -33,10 +37,11 @@ if __name__ == "__main__":
     lista_macro_solved = tb.solve_hierarchy_in_list(lista_billing + lista_avulsos)
     
     # Processamento Condicional
+    lista_billing_solda_iterna = tb.solve_internal_welds(lista_billing)
+    lista_billing_solda_iterna, weld_kit = tb.separate_weld_kit_items(lista_billing)
+    loose_items = tb.get_only_loose_items_from_weld_kit(weld_kit)
     if solda_usinados_interna:
-        lista_billing = tb.solve_internal_welds(lista_billing)
-        lista_billing, weld_kit = tb.separate_weld_kit_items(lista_billing)
-        loose_items = tb.get_only_loose_items_from_weld_kit(weld_kit)
+        lista_billing = lista_billing_solda_iterna
     if pastas_de_solda:
         for conjunto in lista_billing:
             tb.copiar_arquivos_solda_conjuntos(acervo, destino, conjunto)
@@ -48,7 +53,7 @@ if __name__ == "__main__":
     df1 = pd.DataFrame.from_dict(data_list)
     df2 = pd.DataFrame.from_dict(lista_macro_solved)
     df3 = pd.DataFrame.from_dict(lista_billing_solved)
-    filename2 = current_time + '_lista_de_orcamento.xlsx'
+    filename2 = current_time + "_" + nome_planilha_csv + '_lista_de_orcamento.xlsx'
     Sheet1 = "Lista Original"
     Sheet2 = "Lista Macro"
     Sheet3 = "Lista de Orçamento"
